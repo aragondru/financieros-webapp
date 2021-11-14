e3e56d17ee625c6de9602e11b47bbf6faebbcd68
Alejandro Aragon <ar17092@ues.edu.sv>
2021-11-14 13:12:16 -0600

    Init proyecto de financieros

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    
+    <toolbar></toolbar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Toolbar from './components/Toolbar.vue'
+export default {
+  name: 'App',
+
+  components:{
+    Toolbar
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <v-toolbar dense dark>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>ANF WebApp</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items>
+            <v-btn 
+            text 
+            dark 
+            class="hidden-xs-only"
+            v-for="item in menuItems"
+            :key="item.title"
+            >
+            {{item.title}}
+            </v-btn>
+        </v-toolbar-items>
+        <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+        </v-toolbar>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Toolbar',
+        data() {
+            return {
+                menuItems:[
+                    {title:"Inicio"},
+                    {title:"Balance general"},
+                    {title:"Estado de resultados"}
+                ],
+                analisisItems:[
+                    {title:"Razones financiera"},
+                    {title:"Dupont"}
+                ],
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
