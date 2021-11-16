ea5fc28a00fe8e6682cd3ac79917d7830827d16d
avalos <avalos@centralSystems.com>
2021-11-16 00:30:21 -0600

    modificacion en tabla

--- src/components/balance/Tabla.vue
+++ src/components/balance/Tabla.vue
@@ -1,53 +1,68 @@
 <template>
   <v-container dark>
     <v-simple-table dense>
-        <template v-slot:default>
+      <template v-slot:default>
         <thead>
-            <tr>
-            <th class="text-left">
-                Cuenta
-            </th>
-            <th class="text-left">
-                2020
-            </th>
-            <th class="text-left">
-              2019
-            </th>
-            <th class="text-left">
-              2018
-            </th>
-            </tr>
+          <tr>
+            <th class="text-left">Cuenta</th>
+            <th class="text-left">2020</th>
+            <th class="text-left">2019</th>
+            <th class="text-left">2018</th>
+          </tr>
         </thead>
         <tbody>
-            <tr v-for="(activo,key) in balance" :key="key">
-              <td>{{activo.year1.balance.activos}}</td>
-              <td>{{activo.year2.balance.activos}}</td>
-              <td>{{activo.year3.balance.activos}}</td>
-            </tr>
-            
+          <tr>
+            Activos
+          </tr>
+
+          <tr v-for="(cuenta, key) in balance1[0][0]" :key="key">
+            <td>{{ key }}</td>
+            <td>{{ cuenta }}</td>
+            <div v-for="(cuenta2, key2) in balance2[0][0]" :key="key2">
+              <td v-if="key2 == key">{{ cuenta2 }}</td>
+            </div>
+            <div v-for="(cuenta3, key3) in balance3[0][0]" :key="key3">
+              <td v-if="key3 == key1">{{ cuenta3 }}</td>
+            </div>
+          </tr>
+
+          <tr>
+            Pasivos
+          </tr>
+          <tr v-for="(cuenta, key) in balance1[1][0]" :key="key">
+            <td>{{ key }}</td>
+            <td>{{ cuenta }}</td>
+          </tr>
+          <tr>
+            Capital
+          </tr>
+          <tr v-for="(cuenta, key) in balance1[2][0]" :key="key">
+            <td>{{ key }}</td>
+            <td>{{ cuenta }}</td>
+          </tr>
         </tbody>
-        </template>
+      </template>
     </v-simple-table>
   </v-container>
 </template>
 
 <script>
-import estadosF from '../../datos'
-  export default {
-      name:'Tabla',
-      data () {
-        return {
-         balance: estadosF
-        }
-      },
-      methods: {
-        imprimir(){
-          
-        }
-      },
-  }
+import { balance_year1 } from "../../datos";
+import { balance_year2 } from "../../datos";
+import { balance_year3 } from "../../datos";
+export default {
+  name: "Tabla",
+  data() {
+    return {
+      balance1: balance_year1,
+      balance2: balance_year2,
+      balance3: balance_year3,
+    };
+  },
+  methods: {
+    imprimir() {},
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
--- src/datos.js
+++ src/datos.js
@@ -1,104 +1,222 @@
-const estadosF={
-    year1:{
-        balance:{
-            activos:{
-                cajasbancos:266874.3,
-                reportes:6216.4,
-                inversionesF:173026.0,
-                carteraP:882915.2,
-                bienesR:7822.1,
-                inversionesA:114.3,
-                diversos:7704.0,
-                bienesM:16997.6
-            },
-            pasivos:{
-                depositosC:1059104.8,
-                prestamosB:38033.7,
-                prestamosO:79372.0,
-                reportos:0,
-                diversos1:35856.9,
-                cuentasP:6822.7,
-                provisiones:3829.0,
-                diversos2:7132.4
-            },
-            capital:{
-                capitalP:73434.3,
-                reserva:58084.1
-            }
-        },
-        er:{
-            ingresos:{},
-            costos:{},
-            reservas:{},
-        }
-    },
-    year2:{
-        balance:{
-            activos:{
-                cajasbancos:208977.3,
-                reportes:1476.8,
-                inversionesF:77948.5,
-                carteraP:790729.8,
-                bienesR:5217.2,
-                inversionesA:114.3,
-                diversos:5874.7,
-                bienesM:16613.8
-            },
-            pasivos:{
-                depositosC:796996.9,
-                prestamosB:54228.9,
-                prestamosO:72778.7,
-                reportos:13511.4,
-                diversos1:29031.4,
-                cuentasP:8359.7,
-                provisiones:3852.6,
-                diversos2:6239.4
-            },
-            capital:{
-                capitalP:45029.5,
-                reserva:76923.9
-            }
-        },
-        er:{
-            ingresos:{},
-            costos:{},
-            reservas:{},
-        }
-    },
-    year3:{
-        balance:{
-            activos:{
-                cajasbancos:266874.3,
-                reportes:6216.4,
-                inversionesF:173026.0,
-                carteraP:882915.2,
-                bienesR:7822.1,
-                inversionesA:114.3,
-                diversos:7704.0,
-                bienesM:16997.6
-            },
-            pasivos:{
-                depositosC:1059104.8,
-                prestamosB:38033.7,
-                prestamosO:79372.0,
-                reportos:0,
-                diversos1:35856.9,
-                cuentasP:6822.7,
-                provisiones:3829.0,
-                diversos2:7132.4
-            },
-            capital:{
-                capitalP:73434.3,
-                reserva:58084.1
-            }
-        },
-        er:{
-            ingresos:{},
-            costos:{},
-            reservas:{},
-        }
-    },
-}
+let activos_balance_y1 = [{
+    "cajasbancos": 266874.3,
+    "reportes": 6216.4,
+    "inversionesF": 173026.0,
+    "carteraP": 882915.2,
+    "bienesR": 7822.1,
+    "inversionesA": 114.3,
+    "diversos": 7704.0,
+    "bienesM": 16997.6
+}];
+let pasivos_balance_y1 = [{
+    "depositosC": 1059104.8,
+    "prestamosB": 38033.7,
+    "prestamosO": 79372.0,
+    "reportos": 0,
+    "diversos1": 35856.9,
+    "cuentasP": 6822.7,
+    "provisiones": 3829.0,
+    "diversos2": 7132.4
+}]
+
+let capital_balance_y1 = [{
+    "capitalP": 73434.3,
+    "reserva": 58084.1
+}];
+export let balance_year1 = new Array(activos_balance_y1, pasivos_balance_y1, capital_balance_y1);
 
 
-export default estadosF;
+let ingresos_er_y1 = [{}];
+
+let costos_er_y1 = [{}];
+
+let reservas_er_y1 = [{}];
+
+let er_year1 = new Array(ingresos_er_y1, costos_er_y1, reservas_er_y1);
+
+export let year1 = new Array(balance_year1, er_year1);
+
+/*******************************year 2********************************** */
+
+let activos_balance_y2 = [{
+    "cajasbancos": 208977.3,
+    "reportes": 1476.8,
+    "inversionesF": 77948.5,
+    "carteraP": 790729.8,
+    "bienesR": 5217.2,
+    "inversionesA": 114.3,
+    "diversos": 5874.7,
+    "bienesM": 16613.8
+}];
+let pasivos_balance_y2 = [{
+    "depositosC": 796996.9,
+    "prestamosB": 54228.9,
+    "prestamosO": 72778.7,
+    "reportos": 13511.4,
+    "diversos1": 29031.4,
+    "cuentasP": 8359.7,
+    "provisiones": 3852.6,
+    "diversos2": 6239.4
+}]
+
+let capital_balance_y2 = [{
+    "capitalP": 45029.5,
+    "reserva": 76923.9
+}];
+export let balance_year2 = new Array(activos_balance_y2, pasivos_balance_y2, capital_balance_y2);
+
+
+let ingresos_er_y2 = [{}];
+
+let costos_er_y2 = [{}];
+
+let reservas_er_y2 = [{}];
+
+let er_year2 = new Array(ingresos_er_y2, costos_er_y2, reservas_er_y2);
+
+export let year2 = new Array(balance_year2, er_year2);
+
+/*******************************year 3********************************** */
+
+let activos_balance_y3 = [{
+    "cajasbancos": 266874.3,
+    "reportes": 6216.4,
+    "inversionesF": 173026.0,
+    "carteraP": 882915.2,
+    "bienesR": 7822.1,
+    "inversionesA": 114.3,
+    "diversos": 7704.0,
+    "bienesM": 16997.6
+}];
+let pasivos_balance_y3 = [{
+    "depositosC": 1059104.8,
+    "prestamosB": 38033.7,
+    "prestamosO": 79372.0,
+    "reportos": 0,
+    "diversos1": 35856.9,
+    "cuentasP": 6822.7,
+    "provisiones": 3829.0,
+    "diversos2": 7132.4
+}]
+
+let capital_balance_y3 = [{
+    "capitalP": 73434.3,
+    "reserva": 58084.1
+}];
+export let balance_year3 = new Array(activos_balance_y3, pasivos_balance_y3, capital_balance_y3);
+
+
+let ingresos_er_y3 = [{}];
+
+let costos_er_y3 = [{}];
+
+let reservas_er_y3 = [{}];
+
+let er_year3 = new Array(ingresos_er_y3, costos_er_y3, reservas_er_y3);
+
+export let year3 = new Array(balance_year3, er_year3);
+/********************************************************************************************************************** */
+/*const estadosF = {
+    year1: {
+        balance: {
+            activos: {
+                cajasbancos: 266874.3,
+                reportes: 6216.4,
+                inversionesF: 173026.0,
+                carteraP: 882915.2,
+                bienesR: 7822.1,
+                inversionesA: 114.3,
+                diversos: 7704.0,
+                bienesM: 16997.6
+            },
+            pasivos: {
+                depositosC: 1059104.8,
+                prestamosB: 38033.7,
+                prestamosO: 79372.0,
+                reportos: 0,
+                diversos1: 35856.9,
+                cuentasP: 6822.7,
+                provisiones: 3829.0,
+                diversos2: 7132.4
+            },
+            capital: {
+                capitalP: 73434.3,
+                reserva: 58084.1
+            }
+        },
+        er: {
+            ingresos: {},
+            costos: {},
+            reservas: {},
+        }
+    },
+    year2: {
+        balance: {
+            activos: {
+                cajasbancos: 208977.3,
+                reportes: 1476.8,
+                inversionesF: 77948.5,
+                carteraP: 790729.8,
+                bienesR: 5217.2,
+                inversionesA: 114.3,
+                diversos: 5874.7,
+                bienesM: 16613.8
+            },
+            pasivos: {
+                depositosC: 796996.9,
+                prestamosB: 54228.9,
+                prestamosO: 72778.7,
+                reportos: 13511.4,
+                diversos1: 29031.4,
+                cuentasP: 8359.7,
+                provisiones: 3852.6,
+                diversos2: 6239.4
+            },
+            capital: {
+                capitalP: 45029.5,
+                reserva: 76923.9
+            }
+        },
+        er: {
+            ingresos: {},
+            costos: {},
+            reservas: {},
+        }
+    },
+    year3: {
+        balance: {
+            activos: {
+                cajasbancos: 266874.3,
+                reportes: 6216.4,
+                inversionesF: 173026.0,
+                carteraP: 882915.2,
+                bienesR: 7822.1,
+                inversionesA: 114.3,
+                diversos: 7704.0,
+                bienesM: 16997.6
+            },
+            pasivos: {
+                depositosC: 1059104.8,
+                prestamosB: 38033.7,
+                prestamosO: 79372.0,
+                reportos: 0,
+                diversos1: 35856.9,
+                cuentasP: 6822.7,
+                provisiones: 3829.0,
+                diversos2: 7132.4
+            },
+            capital: {
+                capitalP: 73434.3,
+                reserva: 58084.1
+            }
+        },
+        er: {
+            ingresos: {},
+            costos: {},
+            reservas: {},
+        }
+    },
+}*/
+
+
+//export default year1;
